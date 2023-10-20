a6a8c114873d178cfc06ac502b04aec1008f377f
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-10-20 14:32:43 -0300

    feat: add app initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>hello world</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 };
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  primary: "#DBAC2C",
+  "primary-light": "#F1E9C9",
+  "primary-dark": "#C47F17",
+
+  secondary: "#8047F8",
+  "secondary-light": "#EBE5F9",
+  "secondary-dark": "#4B2995",
+
+  tertiary: "#574F4D",
+  "tertiary-light": "#8D8686",
+  "tertiary-dark": "#403937",
+  "tertiary-darker": "#272221",
+
+  "gray-400": "#D7D5D5",
+  "gray-300": "#E6E5E5",
+  "gray-200": "#EDEDED",
+  "gray-150": "#F3F2F2",
+  "gray-100": "#FAFAFA",
+  white: "#FFFFFF",
+};
